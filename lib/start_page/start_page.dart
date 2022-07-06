af568a7d7e3d179cc08002f55e6488fe83dfabeb
bobcop88 <bobcop88dev@gmail.com>
2022-07-06 15:26:01 +0300

    client-app

--- lib/start_page/start_page.dart
+++ lib/start_page/start_page.dart
@@ -14,22 +14,23 @@ class _StartPageState extends State<StartPage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
+      backgroundColor: Color.fromRGBO(242, 242, 242, 1),
       body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/img_start_page.png',
+            ),
+          fit: BoxFit.cover,
+          opacity: 0.2,
+          ),
+        ),
         child: SafeArea(
           child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 100,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    AppLocalizations.of(context)!.sp_welcome,
-                    style: Theme.of(context).textTheme.headline1,
-                    
-                  ),
-                ],
-              ),
+              Spacer(),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -39,17 +40,11 @@ class _StartPageState extends State<StartPage> {
                   ),
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/img_start_page.png',
-                    height: 200,
-                  ),
-                ],
+              SizedBox(
+                height: 50,
               ),
               Padding(
-                padding: EdgeInsets.all(8.0),
+                padding: EdgeInsets.fromLTRB(30.0, 0, 30.0, 10.0),
                 child: Row(
                   children: [
                     Expanded(
@@ -57,12 +52,54 @@ class _StartPageState extends State<StartPage> {
                         onPressed: () {},
                         child: Text(
                           AppLocalizations.of(context)!.sp_login_btn,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        style: ButtonStyle(
+                          padding: MaterialStateProperty.all(EdgeInsets.all(15)),
+                          shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)))
                         ),
                       ),
                     ),
                   ],
                 ),
               ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          AppLocalizations.of(context)!.sp_register_btn,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Color.fromRGBO(200, 36, 47, 1),
+                          ),
+                        ),
+                        style: ButtonStyle(
+                          padding: MaterialStateProperty.all(EdgeInsets.all(15)),
+                          shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+                          backgroundColor: MaterialStateProperty.all(Colors.white),
+                          side: MaterialStateProperty.all(BorderSide(color: Color.fromRGBO(200, 36, 47, 1)))
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!.sp_developed
+                  ),
+                ],
+              ),
+              
             ],
           ),
         ),
